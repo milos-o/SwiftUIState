7a146fc2ce6e32757923a51d78dc542fb80bb96c
Milos Ostojic <milos.osto11@gmail.com>
2025-03-14 15:28:38 +0100

    Adds new tests

--- Tests/SwiftUIStateTests/SwiftUIStateTests.swift
+++ Tests/SwiftUIStateTests/SwiftUIStateTests.swift
@@ -6,6 +6,8 @@ final class Model: ObservableObject {
     @Published var counter: Int = 0
 }
 
+nonisolated(unsafe) let nestedModel = Model()
+
 extension View {
     func debug(_ f: () -> ()) -> some View {
         f()
@@ -30,6 +32,7 @@ final class SwiftUIStateTests: XCTestCase {
     override func setUp() {
         nestedBodyCount = 0
         contentViewBodyCount = 0
+        nestedModel.counter = 0
     }
 
     func testUpdate() {
@@ -153,5 +156,115 @@ final class SwiftUIStateTests: XCTestCase {
         node.rebuildIfNeeded()
         XCTAssertEqual(contentViewBodyCount, 2)
         XCTAssertEqual(nestedBodyCount, 1)
+    }
+
+    func testUnchangedNestedWithObservedObject() {
+        struct Nested: View {
+            @ObservedObject var model = nestedModel
+            var body: some View {
+                nestedBodyCount += 1
+                return Button("Nested button", action: {})
+            }
+        }
+
+        struct ContentView: View {
+            @ObservedObject var model = Model()
+
+            var body: some View {
+                Button("\(model.counter)") {
+                    model.counter += 1
+                }
+                Nested()
+                    .debug {
+                        contentViewBodyCount += 1
+                    }
+            }
+        }
+
+        let v = ContentView()
+        let node = Node()
+        v.buildNodeTree(node)
+        var button: Button {
+            node.children[0].children[0].view as! Button
+        }
+        XCTAssertEqual(contentViewBodyCount, 1)
+        XCTAssertEqual(nestedBodyCount, 1)
+        button.action()
+        node.rebuildIfNeeded()
+        XCTAssertEqual(contentViewBodyCount, 2)
+        XCTAssertEqual(nestedBodyCount, 1)
+    }
+
+    func testBinding1() {
+        struct Nested: View {
+            @Binding var counter: Int
+            var body: some View {
+                nestedBodyCount += 1
+                return Button("Nested button", action: {})
+            }
+        }
+
+        struct ContentView: View {
+            @ObservedObject var model = Model()
+
+            var body: some View {
+                Button("\(model.counter)") {
+                    model.counter += 1
+                }
+                Nested(counter: $model.counter)
+                    .debug {
+                        contentViewBodyCount += 1
+                    }
+            }
+        }
+
+        let v = ContentView()
+        let node = Node()
+        v.buildNodeTree(node)
+        var button: Button {
+            node.children[0].children[0].view as! Button
+        }
+        XCTAssertEqual(contentViewBodyCount, 1)
+        XCTAssertEqual(nestedBodyCount, 1)
+        button.action()
+        node.rebuildIfNeeded()
+        XCTAssertEqual(contentViewBodyCount, 2)
+        XCTAssertEqual(nestedBodyCount, 2)
+    }
+
+    func testBinding2() {
+        struct Nested: View {
+            @Binding var counter: Int
+            var body: some View {
+                nestedBodyCount += 1
+                return Button("\(counter)", action: { counter += 1 })
+            }
+        }
+
+        struct ContentView: View {
+            @ObservedObject var model = Model()
+
+            var body: some View {
+                Nested(counter: $model.counter)
+                    .debug {
+                        contentViewBodyCount += 1
+                    }
+            }
+        }
+
+        let v = ContentView()
+        let node = Node()
+        v.buildNodeTree(node)
+        var button: Button {
+            node.children[0].children[0].view as! Button
+        }
+        XCTAssertEqual(contentViewBodyCount, 1)
+        XCTAssertEqual(nestedBodyCount, 1)
+        XCTAssertEqual(button.title, "0")
+        button.action()
+        node.rebuildIfNeeded()
+        XCTAssertEqual(contentViewBodyCount, 2)
+        XCTAssertEqual(nestedBodyCount, 2)
+        XCTAssertEqual(button.title, "1")
     }
 }

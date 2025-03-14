91f40af00b3c390e252fcf7fb4467d1e742c90c3
Milos Ostojic <milos.osto11@gmail.com>
2025-03-14 09:01:46 +0100

    Adds new tests for state changes

--- Tests/SwiftUIStateTests/SwiftUIStateTests.swift
+++ Tests/SwiftUIStateTests/SwiftUIStateTests.swift
@@ -6,6 +6,16 @@ final class Model: ObservableObject {
     @Published var counter: Int = 0
 }
 
+extension View {
+    func debug(_ f: () -> ()) -> some View {
+        f()
+        return self
+    }
+}
+
+nonisolated(unsafe) var nestedBodyCount = 0
+nonisolated(unsafe) var contentViewBodyCount = 0
+
 struct ContentView: View {
     @ObservedObject var model = Model()
 
@@ -17,7 +27,12 @@ struct ContentView: View {
 }
 
 final class SwiftUIStateTests: XCTestCase {
-    func testExample() {
+    override func setUp() {
+        nestedBodyCount = 0
+        contentViewBodyCount = 0
+    }
+
+    func testUpdate() {
         let v = ContentView()
         let node = Node()
         v.buildNodeTree(node)
@@ -28,5 +43,78 @@ final class SwiftUIStateTests: XCTestCase {
         button.action()
         node.rebuildIfNeeded()
         XCTAssertEqual(button.title, "1")
+    }
+
+    func testConstantNested() {
+        struct Nested: View {
+            var body: some View {
+                nestedBodyCount += 1
+                return Button("Nested button", action: {})
+            }
+        }
+
+        struct ContentView: View {
+            @ObservedObject var model = Model()
+
+            var body: some View {
+                Button("\(model.counter)") {
+                    model.counter += 1
+                }
+                Nested()
+                    .debug {
+                        contentViewBodyCount += 1
+                    }
+            }
+        }
+
+        let v = ContentView()
+        let node = Node()
+        v.buildNodeTree(node)
+        var button: Button {
+            node.children[0].children[0].view as! Button
+        }
+        XCTAssertEqual(contentViewBodyCount, 1)
+        XCTAssertEqual(nestedBodyCount, 1)
+        button.action()
+        node.rebuildIfNeeded()
+        XCTAssertEqual(contentViewBodyCount, 2)
+        XCTAssertEqual(nestedBodyCount, 1)
+    }
+
+    func testChangedNested() {
+        struct Nested: View {
+            var counter: Int
+            var body: some View {
+                nestedBodyCount += 1
+                return Button("Nested button", action: {})
+            }
+        }
+
+        struct ContentView: View {
+            @ObservedObject var model = Model()
+
+            var body: some View {
+                Button("\(model.counter)") {
+                    model.counter += 1
+                }
+                Nested(counter: model.counter)
+                    .debug {
+                        contentViewBodyCount += 1
+                    }
+            }
+        }
+
+        let v = ContentView()
+        let node = Node()
+        v.buildNodeTree(node)
+        var button: Button {
+            node.children[0].children[0].view as! Button
+        }
+        XCTAssertEqual(contentViewBodyCount, 1)
+        XCTAssertEqual(nestedBodyCount, 1)
+        button.action()
+        node.rebuildIfNeeded()
+        XCTAssertEqual(contentViewBodyCount, 2)
+        XCTAssertEqual(nestedBodyCount, 2)
     }
 }

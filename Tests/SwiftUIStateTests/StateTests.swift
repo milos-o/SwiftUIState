f274f5b165927f308f1b41f4707632ad880e9cd4
Milos Ostojic <milos.osto11@gmail.com>
2025-03-17 17:22:17 +0100

    Adds new test

--- Tests/SwiftUIStateTests/StateTests.swift
+++ Tests/SwiftUIStateTests/StateTests.swift
@@ -9,7 +9,6 @@ final class StateTests: XCTestCase {
 
     func testSimple() {
         struct Nested: View {
-            var parentValue: Int
             @State var counter = 0
             var body: some View {
                 Button("\(counter)") {
@@ -24,7 +23,7 @@ final class StateTests: XCTestCase {
                 Button("\(counter)") {
                     counter += 1
                 }
-                Nested(parentValue: counter)
+                Nested()
             }
         }
 
@@ -44,17 +43,49 @@ final class StateTests: XCTestCase {
         XCTAssertEqual(nestedButton.title, "0")
         
         nestedButton.action()
-        nestedNode.needsRebuild = true // todo this needs to be automatic
         node.rebuildIfNeeded()
 
         XCTAssertEqual(button.title, "0")
         XCTAssertEqual(nestedButton.title, "1")
 
         button.action()
-        node.needsRebuild = true // todo this needs to be automatic
         node.rebuildIfNeeded()
 
         XCTAssertEqual(button.title, "1")
+        XCTAssertEqual(nestedButton.title, "1")
+    }
+
+    func testBindings() {
+        struct Nested: View {
+            @Binding var counter: Int
+            var body: some View {
+                Button("\(counter)") {
+                    counter += 1
+                }
+            }
+        }
+
+        struct Sample: View {
+            @State var counter = 0
+            var body: some View {
+                Nested(counter: $counter)
+            }
+        }
+
+        let s = Sample()
+        let node = Node()
+        s.buildNodeTree(node)
+        var nestedNode: Node {
+            node.children[0]
+        }
+        var nestedButton: Button {
+            nestedNode.children[0].view as! Button
+        }
+        XCTAssertEqual(nestedButton.title, "0")
+
+        nestedButton.action()
+        node.rebuildIfNeeded()
+
         XCTAssertEqual(nestedButton.title, "1")
     }
 }
